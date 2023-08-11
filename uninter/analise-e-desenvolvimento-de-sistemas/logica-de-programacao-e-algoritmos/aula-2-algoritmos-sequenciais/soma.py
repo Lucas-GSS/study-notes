8ccf19a7100cdb021c0048fb327cf4396d1c4028
Lucas Silva <lucasgabrieldesouza9@gmail.com>
2023-08-11 17:50:26 -0300

    uninter, aula 2: exercício soma de dois valores

---
+++ uninter/analise-e-desenvolvimento-de-sistemas/logica-de-programacao-e-algoritmos/aula-2-algoritmos-sequenciais/soma.py
@@ -0,0 +1,4 @@
+numero1 = int(input("Insira um número: "))
+numero2 = int(input("Insira outro número: "))
+resultado = numero1 + numero2
+print("A soma dos valores resulta em: {}".format(resultado))
