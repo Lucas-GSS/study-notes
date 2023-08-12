30ec8ddb55afb7a7f1eee64b93e2cc8e36709e67
Lucas Silva <lucasgabrieldesouza9@gmail.com>
2023-08-12 15:26:10 -0300

    uninter, aula 3: exercicio estrutura de decisao simples

---
+++ uninter/analise-e-desenvolvimento-de-sistemas/logica-de-programacao-e-algoritmos/aula-3-estruturas-de-decisao/exercicio-estrutura-simples.py
@@ -0,0 +1,5 @@
+ano_nascimento = int(input('Digite o ano do seu nascimento: '))
+ano_atual = int(input('Digite o ano atual: '))
+
+if (ano_atual - ano_nascimento >= 18):
+    print('Pode tirar hablitação')
