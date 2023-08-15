a6a3c081b84ee74e4e5666d5a9cfa54892760a1d
Lucas Silva <lucasgabrieldesouza9@gmail.com>
2023-08-15 18:30:12 -0300

    uninter, aula 4: desafio tabuada

---
+++ uninter/analise-e-desenvolvimento-de-sistemas/logica-de-programacao-e-algoritmos/aula-4-estrutura-de-repeticao/tabuada.py
@@ -0,0 +1,8 @@
+print('TABUADA')
+numero = int(input('Insira um número: '))
+contador = 0
+while contador <= 10:
+    print(numero, 'x', contador, '=', contador * numero)
+    contador += 1
+
+print('Fim')
