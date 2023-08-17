b1d175aced33bcd05e8fe39023cf529bc511214e
Lucas Silva <lucasgabrieldesouza9@gmail.com>
2023-08-17 13:51:39 -0300

    uninter, aula 4: loop for

---
+++ uninter/analise-e-desenvolvimento-de-sistemas/logica-de-programacao-e-algoritmos/aula-4-estrutura-de-repeticao/loop-for/for.py
@@ -0,0 +1,17 @@
+for i in range(1, 7):
+    # range(start, stop, step)
+    # o range retorna uma sequência de números inteiros
+    # inclui parâmetro start, exclui o parâmetro stop.
+    # Ou seja vai até o valor de stop -1
+    # se somene um parâmetro for fornecido ele será usado como stop
+    # e o start é, por padrão, 0
+    # step negativo a sequencia numérica é decrescente
+    print(i)
+    # printa de 1 a 6
+
+print('-----------------------------------', '\n')
+
+for i in range(10, 0, -1):
+    print(i)
+
+print('Fogo!')
